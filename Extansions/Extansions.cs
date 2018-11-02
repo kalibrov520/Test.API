d44ce7cba0c1b414dbb7f761466f329a13bcafb4
Ilya Kalibrov <kalibrov520@gmail.com>
2018-11-02 12:08:35 +0300

    Global exception handler added

---
+++ Extansions/Extansions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Test.API.Extansions
+{
+    public static class Extansions
+    {
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            // in response we will have an additional header with the message
+            response.Headers.Add("Aplication-Error",message); 
+           
+            //these methods are used only to disaply above message
+            response.Headers.Add("Access-Control-Expose-Headers","Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin","*");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,17 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Test.API.Data;
+using Test.API.Extansions;
 
 namespace Test.API
 {
@@ -50,6 +54,21 @@ namespace Test.API
             else
             {
                 // app.UseHsts();
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            //adds a new header into our response
+                            context.Response.AddApplicationError(error.Error.Message );
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             // app.UseHttpsRedirection();
