3334f343ebd629f9b80899684fd6a83dcce046ed
Ilya Kalibrov <kalibrov520@gmail.com>
2018-11-03 15:05:42 +0300

    Controller for get Users-info added

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Test.API.Data;
+
+namespace Test.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ITestRepository _repo;
+
+        public UsersController(ITestRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            return Ok(user);
+        }
+    }
+ }
