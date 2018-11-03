0ce42ab2c186a55ad5a863eecbaa95f959744932
Ilya Kalibrov <kalibrov520@gmail.com>
2018-11-04 00:40:14 +0300

    Templates for register-required fields added

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -6,25 +6,30 @@ namespace Test.API.Dtos
     public class UserForRegisterDto
     {
         [Required]
+        [RegularExpression("^[0-9A-Za-z]{1,8}$")]
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,8}$")]
         public string Password { get; set; }
         
         [Required]
+        [RegularExpression("^M$|^F$")]
         public string Gender { get; set; }
         
         [Required]
         public DateTime DateOfBirth { get; set; }
         
         [Required]
+        [RegularExpression("^[А-Яа-я]{1,30}")]
         public string FirstName { get; set; }
         
         [Required]
+        [RegularExpression("^[А-Яа-я]{1,30}")]
         public string LastName { get; set; }
         
         [Required]
+        [RegularExpression("^[0-9]{10}")]
         public string PassportNumber { get; set; }
     }
 }
