d86c865c650ffd189dbe0abdb954c8af047c8519
Ilya Kalibrov <kalibrov520@gmail.com>
2018-11-03 18:49:11 +0300

    PagedList class and helper classes implemented

--- Extansions/Extansions.cs
+++ Extansions/Extansions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Test.API.Extansions
 {
@@ -13,6 +14,16 @@ namespace Test.API.Extansions
             //these methods are used only to disaply above message
             response.Headers.Add("Access-Control-Expose-Headers","Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin","*");
+        }
+
+        public static void AddPagination(this HttpResponse response, int currentPage,
+            int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage,
+            itemsPerPage, totalItems, totalPages);
+            
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers","Pagination");
         }
 
         public static int CalculateAge(this DateTime theDateTime)
