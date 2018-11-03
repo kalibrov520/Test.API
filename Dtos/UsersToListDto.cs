e275f8bdad514d12014c255eb9f8534ca46ac472
Ilya Kalibrov <kalibrov520@gmail.com>
2018-11-04 00:41:00 +0300

    Info-fields about users changed

--- Dtos/UsersToListDto.cs
+++ Dtos/UsersToListDto.cs
@@ -4,11 +4,12 @@ namespace Test.API.Dtos
 {
     public class UsersToListDto
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Gender { get; set; }
+        //public string Gender { get; set; }
+        public string PassportNumber { get; set; }
         public int Age { get; set; }
     }
 }
