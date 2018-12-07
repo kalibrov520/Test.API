a44c9c013320e6a0214233a9ddb7c35fa770f169
ikalibrov <kalibrov520@gmail.com>
2018-12-07 14:47:45 +0300

    PhotoController with AddPhoto method

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,7 @@
+namespace Test.API.Controllers
+{
+    public class PhotosController
+    {
+        
+    }
+}
---
+++ Dtos/UserForUpdateDto.cs
@@ -0,0 +1,7 @@
+namespace Test.API.Dtos
+{
+    public class UserForUpdateDto
+    {
+        
+    }
+}
